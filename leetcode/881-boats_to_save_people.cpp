c6f0716264badf51c06e568b9f486747dbbfdbe1
Krishanth K <krishanth.k2006@gmail.com>
2026-04-08 02:08:05 +0530

    less gooo

---
+++ leetcode/881-boats_to_save_people.cpp
@@ -0,0 +1,21 @@
+// tag
+// tag pointers
+
+// diff: med
+
+// url: https://leetcode.com/problems/boats-to-save-people/
+
+#include <iostream>
+#include <vector>
+using std::cout, std::endl, std::vector, std::string;
+
+class Solution {
+  public:
+	int numRescueBoats(vector<int> &people, int limit) {}
+};
+
+int main()
+{
+	Solution soln;
+	return 0;
+}
