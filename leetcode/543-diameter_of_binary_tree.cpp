7982b4d61f9e2597a20fd7cbf7a8242918dac30f
Krishanth K <krishanth.k2006@gmail.com>
2026-09-01 20:49:04 +0530

    update a previous question

--- leetcode/543-diameter_of_binary_tree.cpp
+++ leetcode/543-diameter_of_binary_tree.cpp
@@ -1,45 +1,52 @@
 #include <algorithm>
 #include <iostream>
+#include <unordered_map>
 #include <vector>
 using std::cout, std::endl, std::vector, std::string;
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
- */
+struct TreeNode
+{
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right)
+	    : val(x), left(left), right(right)
+	{
+	}
+};
+
 class Solution {
   public:
 	int maxDia = 0;
+	std::unordered_map<TreeNode *, int> maxDepth{};
 
 	int getDepth(TreeNode *node)
 	{
 		if (!node)
+		{
+			maxDepth[node] = 0;
 			return 0;
+		}
 		if (!(node->right) && !(node->left))
+		{
+			maxDepth[node] = 1;
 			return 1;
+		}
 
 		int leftDepth = getDepth(node->left);
 		int rightDepth = getDepth(node->right);
+
+		maxDia = std::max(maxDia, leftDepth + rightDepth);
 
 		return 1 + std::max(leftDepth, rightDepth);
 	}
 
 	int diameterOfBinaryTree(TreeNode *root)
 	{
-		int leftDia = getDepth(root->left);
-		int rightDia = getDepth(root->right);
-
-		maxDia = std::max(maxDia, leftDia + rightDia);
-
-		return std::max()
+		getDepth(root);
+		return maxDia;
 	}
 };
 
